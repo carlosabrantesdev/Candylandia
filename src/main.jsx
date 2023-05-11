9a11899e06614fd10b4c553411a9a14f38672fee
CarlosH6 <carlosduartee128@gmail.com>
2023-05-11 20:12:36 -0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import Home from "./routes/Home";
-import Vitrine from "./routes/Vitrine"
-import Detalhes from "./routes/Detalhes"
-import Sobre from "./routes/Sobre"
-import Form from "./routes/Form"
+import Vitrine from "./routes/Vitrine";
+import Detalhes from "./routes/Detalhes";
+import Sobre from "./routes/Sobre";
+import Form from "./routes/Form";
 import NaoEncontrado from "./routes/NaoEncontrado";
 import AppContext from './context/AppContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppContext>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/vitrine" element={<Vitrine/>}/>
-        <Route path="/detalhes/:id" element={<Detalhes />} />
-        <Route path="/sobre" element={<Sobre/>}/>
-        <Route path="/form" element={<Form/>}/>
-        <Route path="*" element={<NaoEncontrado/>} />
-      </Routes>
-    </BrowserRouter>
+      <HashRouter basename="/Candylandia">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/vitrine" element={<Vitrine />} />
+          <Route path="/detalhes/:id" element={<Detalhes />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="*" element={<NaoEncontrado />} />
+        </Routes>
+      </HashRouter>
     </AppContext>
-  </React.StrictMode>,
+  </React.StrictMode>
 );

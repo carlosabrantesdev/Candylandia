c8702d619fced3b135174a32d904205705e0aafe
CarlosH6 <carlosduartee128@gmail.com>
2023-05-11 20:37:30 -0300

    fix

--- src/routes/Form.jsx
+++ src/routes/Form.jsx
@@ -62,7 +62,7 @@ function Form() {
         if (usuario) {
           localStorage.setItem('id', usuario.id);
           context.setUser({ user: usuario.user, id: usuario.id });
-          window.location.href = '/Candylandia/vitrine';
+          window.location.href = window.location.pathname + 'vitrine';
         } else {
           setPopup({ texto: 'Usuário ou senha incorretos', cor: 'vermelho' })
           fecharPopup();

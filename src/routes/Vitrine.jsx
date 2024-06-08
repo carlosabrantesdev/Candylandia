34399e0bdd02464d74c9d6ce63755e84046a65ac
Carlos Henrique <114544383+CarlosH6@users.noreply.github.com>
2024-06-08 13:24:16 -0300

    Update Vitrine.jsx

--- src/routes/Vitrine.jsx
+++ src/routes/Vitrine.jsx
@@ -3,66 +3,72 @@ import '../main.css';
 import Card from '../Card';
 import Header from '../Header';
 import Footer from '../Footer';
+import produtosDados from './produtos.json';
 
 function Vitrine() {
   const [produtos, setProdutos] = useState([]);
   const [carregando, setCarregando] = useState(true);
 
-  const produtosCandylandia = produtos.filter(produto => produto?.loja == "Candylandia")
+  const produtosCandylandia = produtos.filter(produto => produto?.loja === "Candylandia");
 
   document.title = "Vitrine";
 
   useEffect(() => {
-    fetch('https://63069afec0d0f2b8011f9944.mockapi.io/produtos')
-      .then(resposta => resposta.json())
-      .then(info => {
-        setProdutos(info);
-        setCarregando(false);
-      });
+    setProdutos(produtosDados);
+    setCarregando(false);
   }, []);
 
   if (carregando) {
-    return <div id='carregando'></div>;
+    return <div id='carregando'>Carregando...</div>;
   }
 
   return (
     <div className='loja'>
       <Header />
-      <h1 id='secao'> Mais vendidos </h1>
-      <div className='cards'>
-        {produtosCandylandia?.slice(12, 16).map((produto) => (
-          <Card
-            imagem={produto.imagem}
-            nome={produto.nome}
-            estrelas={produto.estrelas}
-            preco={produto.preco}
-            id={produto.id}
-          />
-        ))}
+      <div>
+        <h1 id="secao"> Mais vendidos </h1>
+        <div className='cards'>
+          {produtosCandylandia.slice(0, 4).map((produto) => (
+            <Card
+              key={produto.nome}
+              imagem={produto.imagem}
+              nome={produto.nome}
+              estrelas={produto.estrelas}
+              preco={produto.preco}
+              id={produto.id}
+            />
+          ))}
+        </div>
       </div>
-      <h1 id='secao'> Mais bem avaliados </h1>
-      <div className='cards'>
-        {produtosCandylandia?.slice(16, 20).map((produto) => (
-          <Card
-            imagem={produto.imagem}
-            nome={produto.nome}
-            estrelas={produto.estrelas}
-            preco={produto.preco}
-            id={produto.id}
-          />
-        ))}
+      <div>
+        <h1 id="secao"> Mais bem avaliados </h1>
+        <div className='cards'>
+          {produtosCandylandia.slice(4, 8).map((produto) => (
+            <Card
+              key={produto.nome}
+              imagem={produto.imagem}
+              nome={produto.nome}
+              estrelas={produto.estrelas}
+              preco={produto.preco}
+              id={produto.id}
+            />
+          ))}
+        </div>
       </div>
-      <h1 id='secao'>Destaques</h1>
-      <div className='cards'>
-        {produtosCandylandia?.slice(8, 12).map((produto) => (
-          <Card
-            imagem={produto.imagem}
-            nome={produto.nome}
-            estrelas={produto.estrelas}
-            preco={produto.preco}
-            id={produto.id}
-          />
-        ))}
+      <div>
+        <h1 id="secao"> Destaques </h1>
+        <div className='cards'>
+          {produtosCandylandia.slice(8, 12).map((produto) => (
+            <Card
+              key={produto.nome}
+              imagem={produto.imagem}
+              nome={produto.nome}
+              estrelas={produto.estrelas}
+              preco={produto.preco}
+              id={produto.id}
+            />
+          ))}
+        </div>
       </div>
       <Footer />
     </div>
